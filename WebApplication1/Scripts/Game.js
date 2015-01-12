5386715baf16aa5c72f4b4be2c21d1e691912550
Jan Kevin Munar <jankevinmunar@gmail.com>
2015-01-12 13:45:59 -0500

    Added untracked files

---
+++ WebApplication1/Scripts/Game.js
@@ -0,0 +1,15 @@
+﻿var Player = (function () {
+    function Player() {
+        this.strength = 10;
+    }
+    Player.prototype.fights = function () {
+        console.log("Fight!");
+    };
+    return Player;
+})();
+
+function main() {
+    var ice = new Player();
+    ice.fights();
+}
+//# sourceMappingURL=Game.js.map
